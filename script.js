a06f09d4ccb53ec65d407e803209408dbb22357e
SIDIDEV <sidimariko61@gmail.com>
2024-04-05 20:55:12 +0000

    Exo 2 boucle js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-let number = prompt("tape number beetween 0 and 10");
+const guest = 10
 
-if (number > 10 || number < 0) {
-  console.log(
-    "le nombre n'est pas entre 0 et 10. The number was :",
-    number
-  );
-} else {
+let value
 
-  // prompt value by default is string i need to *1 to get a number link (number*1)
-  console.log(typeof (number));
+while (guest !== value) {
+  value = prompt("Guest the number") * 1
 
-  // for (let i = number; i >= 0; i--) {
-  //   console.log(i);
-  // }
-
-  // while (number >= 0) {
-  //   console.log(number);
-  //   number--;
-  // }
+  if (guest !== value) {
+    console.log('Dommage');
+  }
 }
+
+console.log('Bravo');
+
+
