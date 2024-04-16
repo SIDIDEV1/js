47e4b685b6d81ec35a17272d6624be8cf8a85d16
SIDIDEV <sidimariko61@gmail.com>
2024-04-16 11:13:07 +0100

    calculated students notes done

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-function isPalindrome(word) {
-    const reverse = word.split('').reverse().join('')
-    return reverse.toUpperCase() === word.toUpperCase()
-}
-
-const words = {
-    Sidi: false,
-    Sisis: true,
-    kayak: true,
-    mali: false
-}
-
-for (word in words) {
-    if (isPalindrome(word) !== words[word]) {
-        console.error(`isPalindrome(${word}) aurait du renvoyer ${words[word]}`);
+const students = [
+    {
+        name: 'John',
+        notes: [1, 20, 18, 19, 12]
+    },
+    {
+        name: 'Jane',
+        notes: [17, 18, 20, 13, 15]
+    },
+    {
+        name: 'Sophie',
+        notes: [17, 12, 14, 15, 13]
+    },
+    {
+        name: 'Marc',
+        notes: [2, 3, 5, 8, 9]
+    },
+    {
+        name: 'Manon',
+        notes: [18, 17, 18, 19, 12]
     }
+]
+
+const moyenne = (notes) => {
+    let sum = 0
+    for (let note of notes) {
+        sum = sum + note
+    }
+    return sum / notes.length
+}
+
+
+for (let student of students) {
+    student.moyenne = moyenne(student.notes)
+    console.log(student);
 }
