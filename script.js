bb11bd1988bae9481cf880f447a631e684a8a73e
SIDIDEV <sidimariko61@gmail.com>
2024-04-18 17:07:54 +0100

    Timesss js

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
-class Rectangle {
+console.log('Hi..');
+setTimeout(() => {
+    console.log('j\'attendais');
+}, 1000)
 
-    constructor(width, height) {
-        this.width = width
-        this.height = height
-
-        if (width <= 0 || height <= 0) {
-            throw new Error('Vous n\'avez pas le droit d\'avoir une ou des dimensions negatives')
-        }
-    }
-
-    get perimeter() {
-        return (this.width + this.height) * 2
-    }
-
-    get isValid() {
-        return this.width > 0 && this.height > 0
-    }
-
-    isBiggerThan(c) {
-        return this.perimeter > c.perimeter
-    }
-}
-
-
-class Square extends Rectangle {
-    constructor(width) {
-        super(width, width)
-    }
-
-}
-
-try {
-    const width = parseInt(prompt('Tapez la largeur : '), 10)
-    const height = parseInt(prompt('Tapez la hauter : '), 10)
-    const r = new Rectangle(width, height)
-} catch (error) {
-    console.log(error);
-}
-
+console.log('Bye ;p');
