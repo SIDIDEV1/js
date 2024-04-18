e67576853a9fc4824ed0a7f16c358d73c8c0191e
SIDIDEV <sidimariko61@gmail.com>
2024-04-18 18:25:36 +0100

    interval exo

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-console.log('Hi..');
-setTimeout(() => {
-    console.log('j\'attendais');
-}, 1000)
+// const t = setInterval(() => {
+//     console.log(1)
+// }, 1000);
 
-console.log('Bye ;p');
+// setTimeout(() => {
+//     clearInterval(t)
+// }, 5000)
+
+
+function decompte(n) {
+    const t = setInterval(() => {
+        n--
+        console.log(n);
+        if (n === 0) {
+            clearInterval(t)
+        }
+    }, 1000)
+
+
+}
+
+decompte(3)
