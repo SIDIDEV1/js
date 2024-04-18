871dafb768a04921fb53a8125a84bd760f21d1ca
SIDIDEV <sidimariko61@gmail.com>
2024-04-18 23:29:35 +0100

    Fetch with asyn await

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-fetch('https://jsonplaceholder.typicode.com/usedrs')
-    .then(r => r.json())
+async function getUsers() {
+    const r = await fetch('https://jsonplaceholder.typicode.com/users')
+    if (r.ok === true) {
+        return r.json()
+    }
+
+    throw new Error('Nous n\'avons pas pu recuperer les datas!')
+}
+
+
+getUsers()
     .then(body => console.log(body))
-    .catch(e => console.log(e))
+    .catch(e => console.error(e.message))
 
