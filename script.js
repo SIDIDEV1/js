6457ff102b2147daa2ed388ef22cc795c71bee23
SIDIDEV <sidimariko61@gmail.com>
2024-04-18 23:17:26 +0100

    Fetch with js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-console.log("1")
+fetch('https://jsonplaceholder.typicode.com/usedrs')
+    .then(r => r.json())
+    .then(body => console.log(body))
+    .catch(e => console.log(e))
 
-setTimeout(()=> {
-  console.log("2");
-}, 0);
-
-new Promise((resolve) => {
-  console.log("3")
-  resolve()
-})
-  .then(()=> console.log("4"))
-  .finally(() => console.log("5"))
-  .then(()=> console.log("6"))
-
-console.log("7");
-
-
-// 1, 3, 7, 4, 5, 6, 2
