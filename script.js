79dc26d84e176243796ae021e041e34738904466
SIDIDEV <sidimariko61@gmail.com>
2024-04-18 09:59:58 +0100

    exo class find book

--- script.js
+++ script.js
@@ -1,37 +1,66 @@
-class Rectangle {
+class Book {
+    #page = 1
 
-    constructor(width, height) {
-        this.width = width
-        this.height = height
+    constructor(title, pages) {
+        this.title = title
+        this.pages = pages
     }
 
-    get perimeter() {
-        return (this.width + this.height) * 2
+    get page() {
+        return this.#page
     }
 
-    get isValid() {
-        return this.width > 0 && this.height > 0
+    nextPage() {
+        if (this.#page < this.pages) {
+            this.#page++
+        }
     }
 
-    isBiggerThan(c) {
-        return this.perimeter > c.perimeter
+    close() {
+        this.#page = 1
     }
 }
 
 
-class Square extends Rectangle {
-    constructor(width) {
-        super(width, width)
+class Library {
+    #books = []
+
+    addBook(b) {
+        this.#books.push(b)
     }
 
+    addBooks(books) {
+        for (let book of books) {
+            this.addBook(book)
+        }
+    }
+
+    findBooksByLetter(l) {
+        console.log(l);
+        find = []
+        for (let book of this.#books) {
+            if (book.title[0].toLowerCase() === l.toLowerCase()) {
+                find.push(book)
+            }
+        }
+        return find
+    }
 }
 
 
-const r = new Rectangle(10, 20);
-console.log(r.perimeter) // 60
-console.log(r.isValid) // true
-const r2 = new Rectangle(-10, 20);
-console.log(r2.isValid) // false
-const c = new Square(10);
-console.log(c.perimeter) // 40
-console.log(r.isBiggerThan(c)) // true
+
+const b = new Book('Seigneur des anneaux', 200);
+console.log(b.page)
+b.nextPage()
+console.log(b.page)
+b.close()
+console.log(b.page)
+
+const l = new Library()
+l.addBook(b)
+l.addBooks([
+    new Book('Ready player one', 100),
+    new Book('Oui-oui', 10),
+    new Book('Sillage', 50),
+])
+console.log(l.findBooksByLetter(''))
