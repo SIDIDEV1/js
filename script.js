0f14996d52b8749f04ad1ea1951864ad23259e55
SIDIDEV <sidimariko61@gmail.com>
2024-04-19 11:05:24 +0100

    Clean code

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-function addDate(date, days) {
-    const newDate = new Date(date.getTime())
-    newDate.setDate(newDate.getDate() + days)
-    return newDate
-}
-
 const MONTHS = 'months'
 const DAYS = 'days'
 const YEARS = 'years'
 
 function addInterval(date, n, unit) {
     const newDate = new Date(date.getTime())
-    if (unit === MONTHS) {
-        newDate.setMonth(newDate.getMonth() + n)
-        return newDate
-    } else if (unit === DAYS) {
-        newDate.setDate(newDate.getDate() + n)
-        return newDate
-    } else {
-        newDate.setFullYear(newDate.getFullYear() + n)
-        return newDate
+
+    switch (unit) {
+        case MONTHS:
+            newDate.setMonth(newDate.getMonth() + n)
+            break;
+        case DAYS:
+            newDate.setDate(newDate.getDate() + n)
+            break;
+        case YEARS:
+            newDate.setFullYear(newDate.getFullYear() + n)
+            break;
     }
+
+    return newDate
 }
 
 
 const today = new Date()
-const future = addInterval(today, 3, MONTHS)
+const future = addInterval(today, 3, YEARS)
 
 console.log(today);
 console.log(future);
