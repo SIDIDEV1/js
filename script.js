ce73c3a2f256dd2c42232ee9e51b6d8702fc5109
SIDIDEV <sidimariko61@gmail.com>
2024-04-19 12:52:52 +0100

    querySelector

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-const YEARS = 0
-const MONTHS = 1
-const DAYS = 2
+const app = document.querySelector('#app')
 
 
-function addInterval(date, interval) {
-    const parts = [
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate(),
-        date.getHours(),
-        date.getMinutes(),
-        date.getSeconds(),
-        date.getMilliseconds()
-    ]
-    for (const [unit, value] of Object.entries(interval)) {
-        parts[unit] += value
-    }
-    return new Date(...parts)
-}
-
-
-const today = new Date()
-const future = addInterval(today, {
-    [MONTHS]: 1,
-    [YEARS]: 1,
-    [DAYS]: 2,
-})
-
-console.log(today);
-console.log(future);
+console.log(app);
