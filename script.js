3e39cc841ad42029b2e7c29616fb3ad04bf6dcbf
SIDIDEV <sidimariko61@gmail.com>
2024-04-19 11:03:21 +0100

    Date tp 2 change date with addInterval

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ function addDate(date, days) {
     return newDate
 }
 
+const MONTHS = 'months'
+const DAYS = 'days'
+const YEARS = 'years'
+
+function addInterval(date, n, unit) {
+    const newDate = new Date(date.getTime())
+    if (unit === MONTHS) {
+        newDate.setMonth(newDate.getMonth() + n)
+        return newDate
+    } else if (unit === DAYS) {
+        newDate.setDate(newDate.getDate() + n)
+        return newDate
+    } else {
+        newDate.setFullYear(newDate.getFullYear() + n)
+        return newDate
+    }
+}
+
 
 const today = new Date()
-const tomorrow = addDate(today, 1)
+const future = addInterval(today, 3, MONTHS)
 
 console.log(today);
-console.log(tomorrow);
+console.log(future);
